547513eca7e565033d30cb3aa965799acdbdfb66
Jacob Braaten <braatenj@gmail.com>
2021-08-21 13:47:36 -0500

    created launch-mx to make starting from scratch easier. Updated farm-stats.

--- farm-stats.js
+++ farm-stats.js
@@ -6,13 +6,26 @@ const ramCostConstant = 55000;
 
 export async function main(ns) {
     var serverNameList = ns.getPurchasedServers();
+    var totalRam = 0;
+    var totalRamUsed = 0;
+    var totalRamFree = 0;
+    var ramUtilization = 0;
+
     ns.tprint("--==-- Server Farm Stats --==--");
     for (var s = 0; s < serverNameList.length; s++) {
         var box = serverNameList[s];
         var ram = ns.getServerRam(box);
         var maxRam = ram[0];
+        totalRam += maxRam;
         var currentRam = ram[0] - ram[1];
+        totalRamUsed += ram[1];
         var cost = maxRam * ramCostConstant;
         ns.tprint(box + " Ram: " + currentRam + " / " + maxRam + " --==-- Cost: $" + cost);
     }
+    totalRamFree = totalRam - totalRamUsed;
+    ramUtilization = (totalRamUsed / totalRam) * 100
+    ns.tprint("Total Ram: " + totalRam + "GB");
+    ns.tprint("Total Ram Used: " + totalRamUsed + "GB");
+    ns.tprint("Total Ram Free: " + totalRamFree + "GB");
+    ns.tprint("Ram Utilization: " + ramUtilization + "%");
 }
---
+++ launch-mx.js
@@ -0,0 +1,14 @@
+// copy the contents into a script named 'launch-mx.js' and then do 'run launch-mx.js' everything else will be taken care of
+
+export async function main(ns) {
+    let filesToDownload = ["agency-manager.js", "aug-manager.js", "cascade-kill.js", "hack-target.js", "daemon.js", "farm-stats.js", "grow-target.js", "host-manager.js", "node-manager.js", "program-manager.js", "ram-manager.js", "tor-manager.js", "weak-target.js"];
+    let rootURL = "https://raw.githubusercontent.com/braatenj/BitBurnerScripts/master/";
+
+
+    for(var i = 0; i < filesToDownload.length; i++) {
+        await ns.wget(rootURL + filesToDownload[i], filesToDownload[i]);
+        await ns.sleep(200);
+    }
+
+    ns.spawn("daemon.js");
+}
