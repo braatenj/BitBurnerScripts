624436e639a9c60126626c9243ab8a9903a76c0a
Jacob Braaten <braatenj@gmail.com>
2021-08-30 11:45:56 -0500

    found commit-crime issue, accidentally had loop able to bypass sleep causing lockup

--- commit-crime.js
+++ commit-crime.js
@@ -5,12 +5,12 @@ export async function main(ns) {
     while(shouldCommitCrime) {
         if(!ns.isBusy()) {
             ns.commitCrime("shoplift");
-            await ns.sleep(1000);
         }
         if(ns.getServerMoneyAvailable("home") > threshold) {
             ns.print("Money has reached threshold, ceasing crimes");
             shouldCommitCrime = false;
         }
+        await ns.sleep(1000);
         
     }
 }
